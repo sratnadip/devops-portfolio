3010ac7399eef169f5394197d668a1d71c10278d
Ratnadip Shinde <shinderatnadip7727@gmail.com>
2026-01-03 17:16:18 +0530

    Updates

---
+++ _next/static/Jg5R0q9EzgrKfTcaXByFa/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/devops-portfolio//_next/:path+",
+        "destination": "/devops-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
